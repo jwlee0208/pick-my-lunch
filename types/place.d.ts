0c012ee97f962dbebef83ad8f342ef86e4a5f0b7
jinwon <jwlee0208@gmail.com>
2025-06-01 23:00:41 +0900

    2025.06.01 commit - 다국어 ja에 대한 지도 연동 수정 등

--- types/place.d.ts
+++ types/place.d.ts
@@ -2,11 +2,13 @@ export interface Place {
   place_id: string
   name: string
   formatted_address?: string
-  geometry: { location: { lat: number; lng: number } }
+  geometry: {
+    location: { lat: number; lng: number } | google.maps.LatLng
+  }
   business_status?: string
   rating?: number
   vicinity?: string
   distance?: number | null
-  source: 'kakao' | 'google' // API 소스 구분
-  place_url?: string // Kakao Maps 상세 페이지 URL
+  source: 'kakao' | 'google'
+  place_url?: string
 }
