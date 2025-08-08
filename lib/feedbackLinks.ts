bd76a6037bc6381e0babeca1f6ac93755cfc64bb
LEE JINWON <jwlee0208@plitsoft.com>
2025-08-08 16:45:24 +0900

    2025.08.08 commit - add feedback button (multi language) - add google docs urls etc,.

---
+++ lib/feedbackLinks.ts
@@ -0,0 +1,7 @@
+export const feedbackLinks: Record<string, string> = {
+  ko: "https://forms.gle/ko7cNiWEFs5htJHS7",
+  en: "https://forms.gle/6s4utYBYzHbnToeL6",
+  ja: "https://forms.gle/6s4utYBYzHbnToeL6",
+};
+
+
