ddb27e69d918a472d73e880299b607bda5f58226
LEE JINWON <jwlee0208@gmail.com>
2025-08-09 11:30:32 +0900

    feedbackLinks.ts 업데이트

--- lib/feedbackLinks.ts
+++ lib/feedbackLinks.ts
@@ -1,5 +1,5 @@
 export const feedbackLinks: Record<string, string> = {
-  ko: "https://forms.gle/ko7cNiWEFs5htJHS7",
+  ko: "https://forms.gle/MvTEMrGSo8RnJtBa8",
   en: "https://forms.gle/6s4utYBYzHbnToeL6",
   ja: "https://forms.gle/6s4utYBYzHbnToeL6",
 };
